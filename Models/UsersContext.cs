90f1cd906b80d7b694f10d665924fcedd88e146e
Yunda Yin <hentaizephyr@gmail.com>
2020-03-20 11:04:05 +1300

    Added Users.cs and UsersContext.cs in the Model Folder

---
+++ Models/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    
+    public class UsersContext : DbContext
+    {
+
+
+        public UsersContext(DbContextOptions<UsersContext> options)
+               : base(options)
+        {
+        }
+
+        public DbSet<Users> User { get; set; }
+    }
+}
