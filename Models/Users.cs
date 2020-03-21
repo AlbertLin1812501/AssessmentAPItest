1246d07f1b735b787f9a80ca94b40e52972f8a6b
Vincent-Y-Yin <57467501+Vincent-Y-Yin@users.noreply.github.com>
2020-03-22 11:24:22 +1300

    Update Users.cs

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,6 +7,7 @@ namespace WebAPI.Models
 {
     public class Users
     {
+        //Define variables in "Users" section
         public long Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
