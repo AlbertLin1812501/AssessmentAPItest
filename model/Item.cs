fc532273ba5dc094bad1532725fb6a058ca2642d
Haiqin Zhao <i1811411@nzse.ac.nz>
2020-03-22 14:09:19 +1300

    Item

---
+++ model/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+	public class Item
+	{
+        public double ItemId { get; set; }
+        public int ItemPrices { get; set; }
+        public string ItemDescription { get; set; }
+        public string ItemSellerName { get; set; }
+        public string ItemAddress { get; set; }
+        public int ItemContectNum { get; set; }
+    }
+}
