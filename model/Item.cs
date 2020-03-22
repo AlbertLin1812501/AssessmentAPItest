0362292ab7a9a3573e0d07abb07dd2aefe703098
Haiqin Zhao <i1811411@nzse.ac.nz>
2020-03-22 14:19:14 +1300

    Item

--- model/Item.cs
+++ model/Item.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebAPI.Models
+namespace WebAPI.model
 {
 	public class Item
 	{
         public double ItemId { get; set; }
-        public int ItemPrices { get; set; }
+        public double ItemPrices { get; set; }
         public string ItemDescription { get; set; }
         public string ItemSellerName { get; set; }
         public string ItemAddress { get; set; }
--- model/ItemCont.cs
+++ model/ItemCont.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace WebAPI.Models 
+namespace WebAPI.model 
 { 
 public class ItemCon : DbContext
 {
